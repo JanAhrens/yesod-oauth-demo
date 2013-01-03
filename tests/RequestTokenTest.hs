3ca01277079957db5090247b83e9b1ffe869603b
Jan Ahrens <jan.ahrens+git@googlemail.com>
2013-01-03 07:33:33 +0100

    test remaining current request token behavior (closes #3)

--- tests/RequestTokenTest.hs
+++ tests/RequestTokenTest.hs
@@ -7,7 +7,18 @@ import TestImport
 
 requestTokenSpecs :: Specs
 requestTokenSpecs =
-  describe "RequestToken" $
-    it "can successfully create request tokens" $ do
+
+  describe "creating request tokens" $ do
+
+    it "can successfully be created" $ do
       post_ "/request_token"
       statusIs 201
+
+    it "confirms the callback domain" $ do
+      post_ "/request_token"
+      bodyContains "oauth_callback_confirmed=true"
+
+    it "returns oauth token and secret" $ do
+      post_ "/request_token"
+      bodyContains "oauth_token="
+      bodyContains "oauth_token_secret="
